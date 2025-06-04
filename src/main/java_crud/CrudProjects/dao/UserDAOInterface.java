71f4c91a84a1bfe12eff8229715b79fb7356da46
shew33 <shew33@icloud.com>
2025-06-04 15:58:13 +0300

    первый коммит

---
+++ src/main/java_crud/CrudProjects/dao/UserDAOInterface.java
@@ -0,0 +1,18 @@
+package CrudProjects.dao;
+
+import CrudProjects.model.User;
+import java.util.List;
+
+public interface UserDAOInterface {
+
+    List<User> getAllUsers();
+
+    void saveUser(User user);
+
+    User getUserById(Long id);
+
+    void updateUser(long id, User updatedUser);
+
+    void deleteUser(Long id);
+
+}
